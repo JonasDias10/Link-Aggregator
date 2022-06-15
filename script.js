0f1dd8b6b6e78b1ff687355262527291089f9841
Jonas Dias <zdias1413@gmail.com>
2022-06-14 23:21:49 -0300

    Deleted folder script

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const linkTheme = document.querySelector("#link-theme");
 const btnTheme = document.querySelector(".button-switcher");
 const logoTheme = document.querySelector("#logo-theme");
 
-const lightThemePath = '../styles/light-theme.css';
-const darkThemePath = '../styles/dark-theme.css';
+const lightThemePath = './styles/light-theme.css';
+const darkThemePath = './styles/dark-theme.css';
 
-const logoLightPath = '../assets/icons/logo-light-theme.svg';
-const logoDarkPath = '../assets/icons/logo-dark-theme.svg';
+const logoLightPath = './assets/icons/logo-light-theme.svg';
+const logoDarkPath = './assets/icons/logo-dark-theme.svg';
 
 btnTheme.addEventListener('click', () => {
     let isLightTheme = btnTheme.classList.toggle('light-theme');
