e6052f52e1566cd74571ae920f4ea538f4a49983
Jonas Dias <zdias1413@gmail.com>
2022-06-22 20:37:49 -0300

    Refactoring the Code

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 const linkTheme = document.querySelector("#link-theme");
 const btnTheme = document.querySelector(".button-switcher");
-const logoTheme = document.querySelector("#logo-theme");
 
 const lightThemePath = './styles/light-theme.css';
 const darkThemePath = './styles/dark-theme.css';
-
-const logoLightPath = './assets/icons/logo-light-theme.svg';
-const logoDarkPath = './assets/icons/logo-dark-theme.svg';
 
 btnTheme.addEventListener('click', () => {
     let isLightTheme = btnTheme.classList.toggle('light-theme');
     btnTheme.classList.toggle('dark-theme');
     const isTrue = true;
 
-    if(isLightTheme === isTrue){
+    if(isLightTheme === isTrue)
         linkTheme.setAttribute('href', lightThemePath);
-        logoTheme.setAttribute('src', logoLightPath);
-    }else{
+    else
         linkTheme.setAttribute('href', darkThemePath);
-        logoTheme.setAttribute('src', logoDarkPath);
-    }
 });
